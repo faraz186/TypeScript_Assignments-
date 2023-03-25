6e344a9a92d90faeab57555e7a0ce1b825b75c01
Mohammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-03-26 03:25:41 +0500

    Add files via upload

---
+++ Assignment 02 (Quiz App)/app.ts
@@ -0,0 +1,96 @@
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+
+let TimerOut = () => 
+{
+    return new Promise((res) => 
+    {
+        setTimeout(res, 3000)
+    })
+}
+async function welcome() 
+{
+    let rainbowTitle = chalkAnimation.rainbow("\t\t\t\t Welcome to the QuizApp \n\n");
+    await TimerOut();
+    rainbowTitle.stop();
+}
+
+await welcome()
+
+async function AskQuestion() 
+{
+
+    let answers = await inquirer.prompt([
+    {
+        type: "list",
+        name: "questionNo1",
+        choices: ["JavaScript", "React"],
+        message: " TypeScript is a Subset of ??",
+
+    },
+    {
+        type: "list",
+        name: "questionNo2",
+        choices: ["tsc app.ts", "npm start"],
+        message: "Compile the TS File into JS File run command??",
+
+    },
+    {
+        type: "list",
+        name: "questionNo3",
+        choices: ["npm i -y", "npm install -g -y"],
+        message: "Install npm packages run command?? ",
+    },
+    {
+        type: "list",
+        name: "questionNo4",
+        choices: ["TypeScript", "transferScript"],
+        message: "TS stands for ?? ",
+    },
+    {
+        type: "list",
+        name: "questionNo5",
+        choices: ["vercel", "firebase"],
+        message: "In NextJS which website is best for hosting",
+    }])
+
+
+    if (answers.questionNo1 === "JavaScript") {
+        console.log("Congrats!! Correct answer");
+    }
+
+    else () => {
+        console.log("Incorrect");
+
+    }
+    if (answers.questionNo2 === "tsc app.ts") {
+        console.log("Congrats!! Correct answer");
+    }
+    else {
+        console.log("Incorrect");
+
+    }
+    if (answers.questionNo3 === "npm i -y") {
+        console.log("Congrats!! Correct answer");
+    }
+    else {
+        console.log("Incorrect");
+
+    }
+    if (answers.questionNo4 === "TypeScript") {
+        console.log("Congrats!! Correct answer");
+    }
+    else {
+        console.log("Incorrect");
+
+    }
+    if (answers.questionNo5 === "vercel") {
+        console.log("Congrats!! Correct answer");
+    }
+    else {
+        console.log("Incorrect");
+
+    }
+
+}
+AskQuestion()
